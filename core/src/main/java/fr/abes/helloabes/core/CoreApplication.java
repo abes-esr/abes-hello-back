b359a05390f2ca247192c38e19e0076433cfe418
Duy Son Tran <tran@abes.fr>
2020-11-05 14:35:20 +0100

    Init le core du projet

---
+++ core/src/main/java/fr/abes/helloabes/core/CoreApplication.java
@@ -0,0 +1,13 @@
+package fr.abes.helloabes.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
